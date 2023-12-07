209b85424803e42b2194954b59987ebf13dd21bc
S3RG10K <sklemekhin@gmail.com>
2023-12-07 14:14:44 +0300

    initial commit

---
+++ src/test/java/com/test/demo/Testunit1ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testunit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Ya ZAEBALSYA");
+	}
+
+
+}
